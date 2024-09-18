241d7e6721889ec995221b6d2ac77e12c8ce62a3
Hridey Sharma <139631971+hriday455@users.noreply.github.com>
2024-09-18 18:58:00 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const AccountID = 1234
+let AccountEmail = "hridays2003@gmail.com"
+let AccountPassword = "virat"
+var AcoountCity = "jaipur"
+/*
+var not to use because it does not have scope
+*/
+AccountEmail = "hridays@.com"
+AcoountCity = "Pune"
+console.table([AccountID,AccountEmail,AccountPassword,AcoountCity])
