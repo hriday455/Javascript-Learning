63474a4587fcfa3e479432bbda7b5d070ec070d7
Hridey Sharma <139631971+hriday455@users.noreply.github.com>
2024-09-19 11:50:55 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+let name = "Hridey"
+let age = 21
+let isloggin = false
+// number range 2 to power of 53
+// big int if we want to exceed
+// string =""
+// boolean = true/false
+// null = stand alone value
+// underdefined =
+// symbol = unique
+// object
+console.log(typeof "hridey");
+console.log(typeof age);
+console.log(typeof null); // type of null is object
+console.log(typeof undefined); // type of underfined is undefined
+
+
+
