bfe15cbbca1c532ea1826ea74cc7f542bc8f9a26
Hridey Sharma <139631971+hriday455@users.noreply.github.com>
2024-10-12 09:01:14 +0000

    Comparison

--- comparison.js
+++ comparison.js
@@ -1,14 +1,18 @@
 // console.log(2>1)
 // console.log(2>=1)
-console.log(null == 0)
-console.log(null >= 0)
-console.log(null <=0 );
-console.log(null > 0);
-console.log(0>0);
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <=0 );
+// console.log(null > 0);
+// console.log(0>0);
 
 /*
-reason is that equality check == and c
+reason is that equality check == and comparison > <>= <= work diffrently
+Comparison convert null to a number, treating as a 0. That's why (3) null >=0 is true 
+(1) null > 0 is false  
 */
+// === Strict check
+console.log("2"===2)
 
 
 
